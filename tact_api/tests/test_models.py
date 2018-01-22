fa4089107eda1f49fdfd2f774500a6d9fcc42317
kenyatta <kenyatta@example.com>
2018-01-22 16:30:27 +0100

    Test: Coded Announcement, Task model/view tests

--- tact_api/tests/test_models.py
+++ tact_api/tests/test_models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from django.utils import timezone
 
-from lines.models import Line, Event
+from lines.models import Line, Announcement, Event, Task
 
 
 class LineTestCase(TestCase):
@@ -33,13 +33,13 @@ class LineTestCase(TestCase):
         self.assertNotEqual(old_count, new_count)
 
 
-class EventTestCase(TestCase):
+class AnnouncementTestCase(TestCase):
     """
-    Define the test suite for the Event model.
+    Define the test suite for the Announcement model.
     """
     def setUp(self):
         """
-        Define the even test client and other test variables.
+        Define the announcement test client and other test variables.
         """
         self.user = User.objects.create(username="tactician")
 
@@ -48,7 +48,44 @@ class EventTestCase(TestCase):
         self.line = Line(title=self.line_title, owner=self.user)
         self.line.save()
 
-        # create a new event linked to the new line
+        # create an announcement linked to the new line
+        self.announ_title = "Title goes here"
+        self.announ_desc = "Description goes here."
+        self.announ = Announcement(line=self.line, title=self.announ_title, desc=self.announ_desc,
+                                   owner=self.user)
+
+    def tearDown(self):
+        """
+        Delete created user after test completion.
+        """
+        self.user.delete()
+
+    def test_model_can_create_a_announcement(self):
+        """
+        Test if Announcement model can create an announcement object.
+        """
+        old_count = Announcement.objects.count()
+        self.announ.save()
+        new_count = Announcement.objects.count()
+        self.assertNotEqual(old_count, new_count)
+
+
+class EventTestCase(TestCase):
+    """
+    Define the test suite for the Event model.
+    """
+    def setUp(self):
+        """
+        Define the event test client and other test variables.
+        """
+        self.user = User.objects.create(username="tactician")
+
+        # create and save a new line
+        self.line_title = "Title goes here"
+        self.line = Line(title=self.line_title, owner=self.user)
+        self.line.save()
+
+        # create an event linked to the new line
         self.event_title = "Title goes here"
         self.event_desc = "Description goes here."
         self.event_start = timezone.now()
@@ -62,11 +99,49 @@ class EventTestCase(TestCase):
         """
         self.user.delete()
 
-    def test_model_can_create_an_event(self):
+    def test_model_can_create_a_event(self):
         """
-        Test if event model can create an event object.
+        Test if Event model can create an event object.
         """
         old_count = Event.objects.count()
         self.event.save()
         new_count = Event.objects.count()
+        self.assertNotEqual(old_count, new_count)
+
+
+class TaskTestCase(TestCase):
+    """
+    Define the test suite for the Task model.
+    """
+    def setUp(self):
+        """
+        Define the task test client and other test variables.
+        """
+        self.user = User.objects.create(username="tactician")
+
+        # create and save a new line
+        self.line_title = "Title goes here"
+        self.line = Line(title=self.line_title, owner=self.user)
+        self.line.save()
+
+        # create a task linked to the new line
+        self.task_title = "Title goes here"
+        self.task_desc = "Description goes here."
+        self.task_due = timezone.now()
+        self.task = Task(line=self.line, title=self.task_title, desc=self.task_desc,
+                         start=self.task_due, owner=self.user)
+
+    def tearDown(self):
+        """
+        Delete created user after test completion.
+        """
+        self.user.delete()
+
+    def test_model_can_create_a_task(self):
+        """
+        Test if Task model can create a task object.
+        """
+        old_count = Task.objects.count()
+        self.task.save()
+        new_count = Task.objects.count()
         self.assertNotEqual(old_count, new_count)
--- tact_api/tests/test_views.py
+++ tact_api/tests/test_views.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 from rest_framework.test import APIClient
 from rest_framework import status
 
-from lines.models import Line, Event
+from lines.models import Line, Announcement, Event, Task
 
 
 class LineViewTestCase(TestCase):
@@ -71,6 +71,78 @@ class LineViewTestCase(TestCase):
         self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
 
 
+class AnnouncemenrViewTestCase(TestCase):
+    """
+    Test suite for Announcement API views
+    """
+    def setUp(self):
+        """
+        Define the announcement test client and other test variables.
+        """
+        user = User.objects.create(username="tactician")
+
+        # initialize client and force authentication
+        self.client = APIClient()
+        self.client.force_authenticate(user=user)
+
+        # create and post a new line using authorized user
+        self.line_data = {'title': 'Title goes here'}
+        self.res = self.client.post(reverse('line-list'), self.line_data, format='json')
+
+        # create and post a new event, linked to line created above
+        line = Line.objects.get()
+        self.announ_data = {'line': line.id, 'title': 'Title goes here', 'desc': 'Desc. here'}
+        self.response = self.client.post(reverse('announcement-list'),
+                                         self.announ_data, format='json')
+
+    def test_api_can_create_a_announcement(self):
+        """
+        Test the api has event creation capability.
+        """
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_authorization_is_enforced(self):
+        """
+        Test that the api has user authorization.
+        """
+        announ = Announcement.objects.get()
+        new_client = APIClient()
+        response = new_client.get('/announcements/', kwargs={'pk': announ.id}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_api_can_get_a_announcement(self):
+        """
+        Test the api can get a given announcement.
+        """
+        announ = Announcement.objects.get()
+        response = self.client.get('/announcements/', kwargs={'pk': announ.id}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, announ)
+
+    def test_api_can_update_announcement(self):
+        """
+        Test the api can update a given announcement.
+        """
+        # get the created line, announcement
+        line = Line.objects.get()
+        announ = Announcement.objects.get()
+
+        # perform put request to update event
+        update_announ = {'line': line.id, 'title': 'Title goes here', 'desc': 'Desc. here'}
+        response = self.client.put(
+            reverse('announcement-detail', kwargs={'pk': announ.id}), update_announ, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_api_can_delete_announcement(self):
+        """
+        Test the api can delete an announcement.
+        """
+        announ = Announcement.objects.get()
+        response = self.client.delete(reverse('announcement-detail', kwargs={'pk': announ.id}),
+                                      format='json', follow=True)
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
 class EventViewTestCase(TestCase):
     """
     Test suite for Event API views
@@ -95,7 +167,7 @@ class EventViewTestCase(TestCase):
                            'desc': 'Desc. here', 'start': timezone.now(), 'end': timezone.now()}
         self.response = self.client.post(reverse('event-list'), self.event_data, format='json')
 
-    def test_api_can_create_an_event(self):
+    def test_api_can_create_a_event(self):
         """
         Test the api has event creation capability.
         """
@@ -119,7 +191,7 @@ class EventViewTestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertContains(response, event)
 
-    def test_api_can_update_line(self):
+    def test_api_can_update_event(self):
         """
         Test the api can update a given event.
         """
@@ -140,5 +212,78 @@ class EventViewTestCase(TestCase):
         """
         event = Event.objects.get()
         response = self.client.delete(reverse('event-detail', kwargs={'pk': event.id}),
+                                      format='json', follow=True)
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
+class TaskViewTestCase(TestCase):
+    """
+    Test suite for Task API views
+    """
+    def setUp(self):
+        """
+        Define the task test client and other test variables.
+        """
+        user = User.objects.create(username="tactician")
+
+        # initialize client and force authentication
+        self.client = APIClient()
+        self.client.force_authenticate(user=user)
+
+        # create and post a new line using authorized user
+        self.line_data = {'title': 'Title goes here'}
+        self.res = self.client.post(reverse('line-list'), self.line_data, format='json')
+
+        # create and post a new event, linked to line created above
+        line = Line.objects.get()
+        self.task_data = {'line': line.id, 'title': 'Title goes here', 'desc': 'Desc. here',
+                          'due': timezone.now()}
+        self.response = self.client.post(reverse('task-list'), self.task_data, format='json')
+
+    def test_api_can_create_a_task(self):
+        """
+        Test the api has task creation capability.
+        """
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_authorization_is_enforced(self):
+        """
+        Test that the api has user authorization.
+        """
+        task = Task.objects.get()
+        new_client = APIClient()
+        response = new_client.get('/tasks/', kwargs={'pk': task.id}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_api_can_get_a_task(self):
+        """
+        Test the api can get a given task.
+        """
+        task = Task.objects.get()
+        response = self.client.get('/tasks/', kwargs={'pk': task.id}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, task)
+
+    def test_api_can_update_task(self):
+        """
+        Test the api can update a given task.
+        """
+        # get the created line, task
+        line = Line.objects.get()
+        task = Task.objects.get()
+
+        # perform put request to update event
+        update_task = {'line': line.id, 'title': 'Title goes here', 'desc': 'Desc. here',
+                       'due': timezone.now()}
+        response = self.client.put(
+            reverse('task-detail', kwargs={'pk': task.id}), update_task, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_api_can_delete_task(self):
+        """
+        Test the api can delete a task.
+        """
+        task = Task.objects.get()
+        response = self.client.delete(reverse('task-detail', kwargs={'pk': task.id}),
                                       format='json', follow=True)
         self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
